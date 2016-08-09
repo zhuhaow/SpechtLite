2a65466036f42c5ab0b3f07cf3ff8d753cd0199f
Zhuhao Wang <zhuhaow@gmail.com>
2016-08-09 13:21:58 +0800

    Initial Commit

---
+++ SpechtLite/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SpechtLite
+//
+//  Created by Zhuhao Wang on 8/9/16.
+//  Copyright © 2016 Zhuhao Wang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
