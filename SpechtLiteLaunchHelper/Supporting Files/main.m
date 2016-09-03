321a5fbd3e31bc9bc7124b4b7e8a698b95febdf5
Zhuhao Wang <zhuhaow@gmail.com>
2016-09-03 11:31:28 +0800

    Remove copyright header

--- SpechtLiteLaunchHelper/Supporting Files/main.m
+++ SpechtLiteLaunchHelper/Supporting Files/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  SpechtLiteLaunchHelper
-//
-//  Created by Zhuhao Wang on 8/15/16.
-//  Copyright © 2016 Zhuhao Wang. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, const char * argv[]) {
