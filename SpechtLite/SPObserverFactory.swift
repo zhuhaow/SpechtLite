1a4f2d39c44e4bfcd02d05a605660e048256885a
Zhuhao Wang <zhuhaow@gmail.com>
2016-09-03 11:25:56 +0800

    Some refactor

--- SpechtLite/SPObserverFactory.swift
+++ SpechtLite/SPObserverFactory.swift
@@ -1,26 +1,19 @@
-//
-//  SPObserverFactory.swift
-//  SpechtLite
-//
-//  Created by 周斌佳 on 16/8/30.
-//  Copyright © 2016年 Zhuhao Wang. All rights reserved.
-//
-
 import Cocoa
 import NEKit
 import CocoaLumberjackSwift
 
 class SPObserverFactory: ObserverFactory {
 
-    override func getObserverForAdapterSocket(socket: NEKit.AdapterSocket) -> NEKit.Observer<NEKit.AdapterSocketEvent>? {
+    override func getObserverForAdapterSocket(socket: AdapterSocket) -> Observer<AdapterSocketEvent>? {
         return SPAdapterSocketObserver()
     }
 
     class SPAdapterSocketObserver: Observer<AdapterSocketEvent> {
+
         override func signal(event: AdapterSocketEvent) {
             switch event {
             case .SocketOpened(let socket, let request):
-                DDLogInfo("Request: \(request.host) Type: \(socket.type) Rule: \(request.matchedRule!)")
+                DDLogInfo("Request: \(request.host) Type: \(socket.type) Rule: \(request.matchedRule)")
                 break
             default:
                 break
