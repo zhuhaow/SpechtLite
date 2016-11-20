aea147029951cd8d3202a858c5de1de5d55400d1
Zhuhao Wang <zhuhaow@gmail.com>
2016-11-20 23:55:41 +0800

    the helper won't start specht if the main app is already running

--- SpechtLiteLaunchHelper/AppDelegate.m
+++ SpechtLiteLaunchHelper/AppDelegate.m
@@ -1,22 +1,34 @@
 #import "AppDelegate.h"
 
 @interface AppDelegate ()
-
+    
 @end
 
 @implementation AppDelegate
-
+    
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    NSArray *pathComponents = [[[NSBundle mainBundle] bundlePath] pathComponents];
-    pathComponents = [pathComponents subarrayWithRange:NSMakeRange(0, [pathComponents count] - 4)];
-    NSString *path = [NSString pathWithComponents:pathComponents];
-    NSString *binaryPath = [[NSBundle bundleWithPath:path] executablePath];
-    [[NSWorkspace sharedWorkspace] launchApplication:binaryPath];
-    [NSApp terminate:nil];
+    BOOL alreadyRunning = NO;
+    BOOL isActive = NO;
+    NSArray *running = [[NSWorkspace sharedWorkspace] runningApplications];
+    for (NSRunningApplication *app in running) {
+        if ([[app bundleIdentifier] isEqualToString:@"me.zhuhaow.osx.SpechtLite.LaunchHelper"]) {
+            alreadyRunning = YES;
+            isActive = [app isActive];
+        }
+    }
+    
+    if (!alreadyRunning || !isActive) {
+        NSArray *pathComponents = [[[NSBundle mainBundle] bundlePath] pathComponents];
+        pathComponents = [pathComponents subarrayWithRange:NSMakeRange(0, [pathComponents count] - 4)];
+        NSString *path = [NSString pathWithComponents:pathComponents];
+        NSString *binaryPath = [[NSBundle bundleWithPath:path] executablePath];
+        [[NSWorkspace sharedWorkspace] launchApplication:binaryPath];
+        [NSApp terminate:nil];
+    }
 }
-
+    
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
 }
-
+    
 @end
