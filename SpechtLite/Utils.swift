89c2a9275f1ab12424524a4a9c1680bc3cc9c68e
Zhuhao Wang <zhuhaow@gmail.com>
2016-12-13 23:30:08 +0800

    Show modal asynchronizedly on main queue

--- SpechtLite/Utils.swift
+++ SpechtLite/Utils.swift
@@ -2,7 +2,7 @@ import Cocoa
 
 class Utils {
     static func alertError(_ errorDescription: String) {
-        DispatchQueue.main.sync {
+        DispatchQueue.main.async {
             let alert = NSAlert()
             alert.messageText = errorDescription
             alert.runModal()
