e44ceff33ce469fc6d9968c6d6229c5b37156e31
Felix Liu <felix_fl.liu@icloud.com>
2017-01-11 00:29:50 +0800

    update ProxySettingManager
    Add a function ProxySettingManager.clear(). Currently, if you set
    SpechtLite as system proxy, after you exit SpechtLite you can not
    access the Internet. Because the system proxy is still there. This
    function will clear system proxy when you exit SpechtLite.

--- SpechtLite/Manager/ProxySettingManager.swift
+++ SpechtLite/Manager/ProxySettingManager.swift
@@ -3,19 +3,31 @@ import ReactiveSwift
 
 class ProxySettingManager {
     static let setAsSystemProxy: MutableProperty<Bool> = MutableProperty(false)
-    
+
     static func setUp() {
         setAsSystemProxy.producer.combineLatest(with: ProfileManager.currentProxyPort.producer).startWithValues { enabled, port in
             var port = port
             if !enabled {
                 port = nil
             }
-            
+
             if !ProxyHelper.install() {
                 Utils.alertError("Failed to install helper script to set up system proxy.")
             } else {
                 if !ProxyHelper.setUpSystemProxy(port: port) {
                     Utils.alertError("Failed to set up system proxy.")
+                }
+            }
+        }
+    }
+
+    static func clear() {
+        if setAsSystemProxy.value {
+            if !ProxyHelper.install() {
+                Utils.alertError("Failed to install helper script to clear system proxy.")
+            } else {
+                if !ProxyHelper.setUpSystemProxy(port: nil) {
+                    Utils.alertError("Failed to clear system proxy.")
                 }
             }
         }
